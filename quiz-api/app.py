a930497ebf6503e4b6d16ba4d5fb481561581bbf
Abdelgt1 <abdelgt1412@gmail.com>
2023-12-16 23:05:19 +0100

    backend + docker

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -9,5 +9,10 @@ def hello_world():
 	x = 'hsdh'
 	return f"Hello, {x}"
 
+
+@app.route('/quiz-info', methods=['GET'])
+def GetQuizInfo():
+	return {"size": 0, "scores": []}, 200
+
 if __name__ == "__main__":
     app.run()
