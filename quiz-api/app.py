ee49c043a85cd637ab5dd74070b3127775573535
Abdelgt1 <abdelgt1412@gmail.com>
2024-01-03 16:01:12 +0100

    Login

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -1,5 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
+from utils.jwt_utils import build_token
+import hashlib
+
 
 app = Flask(__name__)
 CORS(app)
@@ -13,6 +16,20 @@ def hello_world():
 @app.route('/quiz-info', methods=['GET'])
 def GetQuizInfo():
 	return {"size": 0, "scores": []}, 200
+
+
+@app.route('/login', methods=['POST'])
+def Login():
+	payload = request.get_json()
+	tried_password = payload['password'].encode('UTF-8')
+	tried_password_hash = hashlib.md5(tried_password).hexdigest()
+	password = "94c0d1d9d64e6b31743ed1fbf685539c"
+	if tried_password_hash == password : 
+		token = build_token()
+		return {'token': token}, 200
+	else : 
+		return 'Unauthorized', 401
+
 
 if __name__ == "__main__":
     app.run()
