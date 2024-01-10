9058d82ff87d7dcc52b4f8ffa607f0188be3a8ae
Victor GERARD <vgvictorgerard@gmail.com>
2024-01-10 18:17:25 +0100

    Avancement Front

---
+++ quiz-ui/src/views/HomePage.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import quizApiService from "@/services/QuizApiService";
+
+onMounted(async () => {
+  console.log("Home page mounted");
+});
+
+const registeredScores = ref([]);
+</script>
+
+<template>
+  <h1>Home page</h1>
+</template>
