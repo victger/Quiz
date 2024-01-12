2f7ae8cffd0fc1d9bca2daf46105d4be8711fad7
vgvictorgerard@gmail.com <vgvictorgerard@gmail.com>
2024-01-12 22:12:36 +0100

    Front

--- quiz-ui/src/views/HomePage.vue
+++ quiz-ui/src/views/HomePage.vue
@@ -3,14 +3,19 @@ import { ref, onMounted } from "vue";
 import quizApiService from "@/services/QuizApiService";
 
 const registeredScores = ref([]);
- 
+
 onMounted(async () => {
   try {
     // Appel du service getQuizInfo() depuis QuizApiService.js
     const response = await quizApiService.getQuizInfo();
 
-    // Stockage de la valeur adéquate dans registeredScores
-    registeredScores.value = response.data.scores; // Assurez-vous que la structure de données est correcte
+    // Assurez-vous que la structure de données est correcte
+    if (Array.isArray(response.data.scores)) {
+      // Stockage de la valeur adéquate dans registeredScores
+      registeredScores.value = response.data.scores;
+    } else {
+      console.error("Invalid data structure for scores:", response.data);
+    }
 
     console.log("Home page mounted");
   } catch (error) {
@@ -20,18 +25,12 @@ onMounted(async () => {
 
 </script>
 
-<style>
-
-</style>
-
 <template>
-
-  <div v-for="scoreEntry in registeredScores" v-bind:key="scoreEntry.date">
-  {{ scoreEntry.playerName }} - {{ scoreEntry.score }}
+  <div v-for="scoreEntry in registeredScores" :key="scoreEntry.date">
+    {{ scoreEntry.playerName }} - {{ scoreEntry.score }}
   </div>
 
   <router-link to="/new-quiz">Démarrer le quiz !</router-link>
 
   <p>Test</p>
-
 </template>
